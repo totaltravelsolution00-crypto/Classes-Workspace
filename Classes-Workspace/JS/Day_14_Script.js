dbe310233bd8bca68b3e290a6964f2aae0a67c46
totaltravelsolution00-crypto <totaltravelsolution00@gmail.com>
2025-12-18 12:11:52 +0530

    Add files via upload

---
+++ Classes-Workspace/JS/Day_14_Script.js
@@ -0,0 +1,93 @@
+// Q-1
+// for(let i = 1; i<=10; i++){
+//     console.log(i);   
+// }
+
+// Q-2
+
+// print even no using for loop
+
+// for(let i = 1; i<=20; i++){
+//     if(i%2 === 0)
+//         console.log(i);   
+//     }
+
+// Q-3
+// print odd no 1 to 15 by using while loop
+
+// let a = 1;
+// while (a<=15){
+//     if(a%2 === 1)
+//     console.log(a);
+//     ++a;
+// }
+
+// Q-4
+// print table of 5 
+
+// for(let i = 1; i<=10; i++){
+//         console.log(i*5);
+// }
+
+// Q-5
+// find the sum of no 10 to 50
+
+// let sum = 0
+
+// for(let a = 10; a<=50; a++){
+//     sum = sum + a
+// }
+// console.log(sum);
+
+// Q-6
+//print no bw 1 to 50 that are divisible by 3
+
+// for (let  i = 1; i<=50; i++){
+//     if(i%3 === 0)
+//         console.log(i);     
+// }
+
+// Q-7
+//print number from 1 to 100 that are divisible by 3 and 5
+
+// for (let i = 1; i<=100; i++){
+//    if(i%3 === 0 && i%5 === 0){
+//     console.log(i);
+//    }
+// }
+
+//Q-7
+//write a loop 1 to 100 that stuck first multlple of 7.
+
+// for(let i = 1; i<=100; i++){
+    
+//     console.log(i);
+//     if(i%7 === 0){
+//         break;
+//     }
+// }
+
+//Q-8
+// print first 5 odd no from 1 to 100.
+// let count = 0;
+// for(let i = 1; i<=100; i++){
+//     if(i%2 === 1){
+//         console.log(i);
+//     count++;
+//     }
+
+//     if(count === 5){
+//         break;
+//     }
+//     }
+
+//imp Q-9
+
+// let num = 12345;
+// let count = 0;
+
+// while (num>0){
+//     count++;
+//     num = Math.floor(num/10);//math.floor ka use ham isliye karte hai jise ham (.) ke baad aane wale value haat jaiga. 
+// }
+// console.log("total digit are:" +count);
