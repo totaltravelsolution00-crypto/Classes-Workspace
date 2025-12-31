f60aa26b8dbb0f35570da18387c870865659758c
totaltravelsolution00-crypto <totaltravelsolution00@gmail.com>
2025-12-31 14:04:29 +0530

    Add files via upload

---
+++ Classes-Workspace/JS/Day_17_Array.js
@@ -0,0 +1,29 @@
+//1.Solution:-
+// let fruit = ["Mango", "Apple", "Banana"]
+// console.log(fruit[1]);
+
+//2.Solution:-
+// let fruit = ["Apple", "Banana"]
+// fruit.push("Mango");
+// fruit.unshift("Pineapple");
+// console.log(fruit);
+
+//3.Solution:-
+// let fruit = ["Apple", "Banana"]
+// fruit[1] = "Kiwi"
+// console.log(fruit);
+
+//4.Solution:-
+// let color = ["Green","Yellow"]
+// color.splice(1,0,"red","blue")
+// console.log(color);
+
+//5.Solution:-
+// let element = [1,2,3,4,5,6,7]
+// let arr = element.slice(2,5)
+// console.log(arr);
+
+//6.Solution:-
+// let arr = ["Zara","Arjun","Mira", "Bhavya"]
+// arr.sort().reverse()
+// console.log(arr);
